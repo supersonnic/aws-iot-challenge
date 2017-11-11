d99b3cf62462d564931d7948c82cc61edb8d9b2d
supersonnic <supper_sonnic@yahoo.com>
2017-11-11 16:27:19 -0600

    Adding js file

---
+++ start.sh
@@ -0,0 +1,18 @@
+# stop script on error
+set -e
+
+# Check to see if root CA file exists, download if not
+if [ ! -f ./root-CA.crt ]; then
+  printf "\nDownloading AWS IoT Root CA certificate from Symantec...\n"
+  curl https://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem > root-CA.crt
+fi
+
+# install AWS Device SDK for NodeJS if not already installed
+if [ ! -d ./node_modules ]; then
+  printf "\nInstalling AWS SDK...\n"
+  npm install aws-iot-device-sdk
+fi
+
+# run pub/sub sample app using certificates downloaded in package
+printf "\nRunning pub/sub sample application...\n"
+node index.js --host-name=ao9xjarl08y9j.iot.us-east-1.amazonaws.com --private-key=omega2-3d0d.private.key --client-certificate=omega2-3d0d.cert.pem --ca-certificate=root-CA.crt
